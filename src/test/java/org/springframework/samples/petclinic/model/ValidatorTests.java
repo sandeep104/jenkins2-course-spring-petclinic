e33e939591cc2cae97a29ee0c0246a804b7663cf
Mehta <sandeep.mehta@thomsonreuters.com>
2017-07-25 16:23:01 +0530

    Break a test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -39,6 +39,7 @@ public class ValidatorTests {
         assertThat(constraintViolations.size()).isEqualTo(1);
         ConstraintViolation<Person> violation = constraintViolations.iterator().next();
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
+        assertThat(violation.getPropertyPath().toString()).isEqualTo("asdf");
         assertThat(violation.getMessage()).isEqualTo("may not be empty");
     }
 
